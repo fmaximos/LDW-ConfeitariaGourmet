ca2467dd5aa57a9429d222fde9eb505703c312d1
fmaximos <fmaximos@gmail.com>
2025-04-29 22:17:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Rola suavemente para as seções
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        window.scrollTo({
+          top: target.offsetTop - 120,
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+  
